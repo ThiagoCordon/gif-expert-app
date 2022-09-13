6fd4f26fbbbfa4a09796b45c4adf869e5b749bd3
ThiagoCordon <thiagocordon8@gmail.com>
2022-09-13 15:02:20 -0300

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Programming"]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+      {categories.map((category) => {
+        return <GifGrid key={category} category={category} />;
+      })}
+    </>
+  );
+};
