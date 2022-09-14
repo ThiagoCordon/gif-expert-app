e40b407447737e0de641870245b01ec673d53942
ThiagoCordon <thiagocordon8@gmail.com>
2022-09-13 22:47:52 -0300

    adding anchors

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,8 +1,8 @@
 export const GifItem = ({ title, url, id }) => {
   return (
-    <div className="card">
+    <a className="card" href={url} target="_blank">
       <img src={url} alt={title} />
       <p>{title}</p>
-    </div>
+    </a>
   );
 };
